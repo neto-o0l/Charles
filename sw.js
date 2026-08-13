b09ccebe68c03bc055dfcd2e500ec583fa2e5538
neto-o0l <manoelnetocantot7@gmail.com>
2026-08-13 10:23:35 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("professor-app").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./Professor_neutro.png",
+        "./Professor_estudando.png",
+        "./Professor_feliz.png",
+        "./Professor_bravo.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
